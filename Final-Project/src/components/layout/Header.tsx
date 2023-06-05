1e15e55a5a1ae11d772e06f3a0a3fc916c747165
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-06-06 01:09:23 +0200

    Center first row of header

--- Final-Project/src/components/layout/Header.tsx
+++ Final-Project/src/components/layout/Header.tsx
@@ -1,12 +1,44 @@
 import React from "react";
 import Icon from "../icon/Icon";
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 10vh;
+`;
+
+const FirstRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+`;
+
+const HiddenIcon = styled.div`
+  visibility: hidden;
+`;
+
+const PaddingOnSettings = styled.div`
+  padding-right: 24px;
+`;
 
 const Header = () => {
   return (
-    <div className="HEADER">
-      <Icon icon="sofascore" color="red" width={132} />
-      <Icon icon="settings" color="green" width={50} height={50} />
-    </div>
+    <StyledHeader>
+      <FirstRow>
+        <HiddenIcon>
+          <Icon icon="settings" color="black" />
+        </HiddenIcon>
+        <Icon icon="sofascore" color="black" width={132} />
+        <PaddingOnSettings>
+          <Icon icon="settings" color="black" />
+        </PaddingOnSettings>
+      </FirstRow>
+    </StyledHeader>
   );
 };
 
