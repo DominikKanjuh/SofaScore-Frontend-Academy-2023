11b689eb01e027078cad5ec1f29af86f9590a43d
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-06-06 00:33:14 +0200

    Fix Icon so that it works

--- Final-Project/src/components/icon/Icon.tsx
+++ Final-Project/src/components/icon/Icon.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 
-type IconName = keyof typeof icons;
-
 const icons = {
   settings: "/assets/icons/settings.svg",
+  sofascore: "/assets/icons/logo_sofascore.svg",
 };
 
+type IconName = keyof typeof icons;
 interface IconProps {
   icon: IconName;
-  size?: string | number;
+  height?: string | number;
+  width?: string | number;
   color?: string;
 }
 
-export const Icon: React.FC<IconProps> = ({ icon, size = 24, color }) => {
-  const iconPath = icons[icon];
+const StyledIcon = styled.img<IconProps>`
+  height: ${({ height }) => (height ? height : "24px")};
+  width: ${({ width }) => (width ? width : "24px")};
+  -webkit-mask-image: ${({ icon }) => `url('${icons[icon]}')`};
+  mask-image: ${({ icon }) => `url('${icons[icon]}')`};
+  -webkit-mask-repeat: no-repeat;
+  -webkit-mask-size: contain;
+  -webkit-mask-position: center center;
+  mask-repeat: no-repeat;
+  background-color: ${({ color }) => color};
+`;
 
-  return <img src={iconPath} width={size} height={size} color={color} />;
+export const Icon: React.FC<IconProps> = ({ icon, height, width, color }) => {
+  return <StyledIcon icon={icon} width={width} height={height} color={color} />;
 };
 
 export default Icon;
--- Final-Project/src/components/layout/Header.tsx
+++ Final-Project/src/components/layout/Header.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import SofascoreSVG from "../../../public/assets/SofascoreSVG";
 import Icon from "../icon/Icon";
 
 const Header = () => {
   return (
     <div className="HEADER">
-      <SofascoreSVG color={"black"} width="132" />
-      <Icon icon="settings" color="red" size={100} />
+      <Icon icon="sofascore" color="red" width={132} />
+      <Icon icon="settings" color="green" width={50} height={50} />
     </div>
   );
 };
