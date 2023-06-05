4823ea8cd53ac25cf1f069420512d50cc71bca60
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-06-06 01:23:07 +0200

    Add colors to theme.ts

--- Final-Project/src/components/theme/Theme.ts
+++ Final-Project/src/components/theme/Theme.ts
@@ -1,18 +1,90 @@
 export type ThemeType = typeof light;
 
 export const light = {
-  primary: {
-    default: "#374df5",
-    variant: "#1327ba",
-    highlight: "#e1edfe",
+  color: {
+    primary: {
+      default: "#374df5",
+      variant: "#1327ba",
+      highlight: "#e1edfe",
+    },
+    secondary: {
+      default: "#f0eedf",
+      variant: "#dddbc9",
+      highlight: "#f7f6ef",
+    },
+    onColor: {
+      primary: "#ffffff",
+      secondary: "rgba(255, 255, 255, 0.6)",
+    },
+    surface: {
+      0: "#eff3f8",
+      1: "#ffffff",
+      2: "rgba(192, 207, 228, 0.2)",
+    },
+    onSurface: {
+      lv1: "#121212",
+      lv2: "rgba(18, 18, 18, 0.4)",
+      lv3: "rgba(18, 18, 18, 0.2)",
+      lv4: "rgba(18, 18, 18, 0.1)",
+    },
+    status: {
+      error: "#ea4545",
+      alert: "#d8b127",
+      success: "#1da86d",
+    },
+    specific: {
+      live: "#e93030",
+    },
+  },
+  layer: {
+    elevation: {
+      1: "#ffffff",
+      2: "#ffffff",
+    },
   },
 };
 
 export const dark = {
-  primary: {
-    default: "#849afd",
-    variant: "#afbeff",
-    highlight: "#2d385c",
+  color: {
+    primary: {
+      default: "#849afd",
+      variant: "#afbeff",
+      highlight: "#2d385c",
+    },
+    secondary: {
+      default: "#3a3a39",
+      variant: "#404039",
+      highlight: "#171616",
+    },
+    onColor: {
+      primary: "#ffffff",
+      secondary: "rgba(255, 255, 255, 0.6)",
+    },
+    surface: {
+      0: "#101114",
+      1: "#21242d",
+      2: "rgba(0, 0, 0, 0.4)",
+    },
+    onSurface: {
+      lv1: "#ffffff",
+      lv2: "rgba(255, 255, 255, 0.4)",
+      lv3: "rgba(255, 255, 255, 0.2)",
+      lv4: "rgba(255, 255, 255, 0.1)",
+    },
+    status: {
+      error: "#f26666",
+      alert: "#ecca51",
+      success: "#30b87f",
+    },
+    specific: {
+      live: "#f14b4b",
+    },
+  },
+  layer: {
+    elevation: {
+      1: "#2b2e38",
+      2: "#2b2e38",
+    },
   },
 };
 
