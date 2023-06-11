f6d9312ea8c82b40e9fb431564ce13c57af36433
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-06-11 21:06:04 +0200

    Final

---
+++ Final-Project/src/components/container/Container.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.08);
+  border-radius: 16px;
+`;
+
+const Container = () => {
+  return <div>Container</div>;
+};
+
+export default Container;
